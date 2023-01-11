bd2cd58516e749e6ca30573b882173095af89e71
ngoctd <trangoc.contact@gmail.com>
2023-01-11 10:13:57 +0700

    u

--- http_client_proxy.go
+++ http_client_proxy.go
@@ -10,24 +10,33 @@ import (
 	"github.com/sony/gobreaker"
 )
 
-type breaker struct {
+type circuitBreaker struct {
 	mapRequestURLToBreaker map[string]*gobreaker.CircuitBreaker
 	mutex                  *sync.Mutex
 	logger                 logger
+	settings               CircuitSetting
 }
 
-func (b breaker) getCircuitBreaker(requestURL string) *gobreaker.CircuitBreaker {
+// CircuitSetting represents settings for gobreaker.CircuitBreaker
+type CircuitSetting struct {
+	MaxRequests         uint32
+	ClosedInterval      time.Duration
+	OpenInterval        time.Duration
+	ConsecutiveFailures uint32
+}
+
+func (b circuitBreaker) getCircuitBreaker(requestURL string) *gobreaker.CircuitBreaker {
 	b.mutex.Lock()
 	defer b.mutex.Unlock()
 	if _, ok := b.mapRequestURLToBreaker[requestURL]; !ok {
 		// not exist, create
 		newCb := gobreaker.NewCircuitBreaker(gobreaker.Settings{
 			Name:        requestURL,
-			MaxRequests: 5,                // MaxRequests pass through cb when state if half-open
-			Interval:    time.Minute,      // Reset counter in open
-			Timeout:     time.Second * 10, // change to half-open when open
+			MaxRequests: b.settings.MaxRequests,    // MaxRequests pass through cb when state if half-open
+			Interval:    b.settings.ClosedInterval, // Reset counter in open
+			Timeout:     b.settings.OpenInterval,   // change to half-open when open
 			ReadyToTrip: func(counts gobreaker.Counts) bool {
-				return counts.ConsecutiveFailures >= 10
+				return counts.ConsecutiveFailures >= b.settings.ConsecutiveFailures
 			},
 			OnStateChange: func(name string, from gobreaker.State, to gobreaker.State) {
 				b.logger.print(fmt.Sprintf("httpclient.circuitbreaker.OnStateChange from %s to %s", from.String(), to.String()))
@@ -42,18 +51,19 @@ func (b breaker) getCircuitBreaker(requestURL string) *gobreaker.CircuitBreaker 
 
 // ClientBreaker represents httpclient instance with Circuit Breaker proxy
 type ClientBreaker struct {
-	client  *Client
-	breaker breaker
+	client *Client
+	cb     circuitBreaker
 }
 
 // NewClientBreaker ...
-func NewClientBreaker(client *Client, loggerFunc loggerFunc) *ClientBreaker {
+func NewClientBreaker(client *Client, circuitSetting CircuitSetting, loggerFunc loggerFunc) *ClientBreaker {
 	breaker := &ClientBreaker{
 		client: client,
-		breaker: breaker{
+		cb: circuitBreaker{
 			mapRequestURLToBreaker: make(map[string]*gobreaker.CircuitBreaker),
 			mutex:                  &sync.Mutex{},
 			logger:                 loggerFunc,
+			settings:               circuitSetting,
 		},
 	}
 
@@ -69,7 +79,7 @@ func NewClientBreaker(client *Client, loggerFunc loggerFunc) *ClientBreaker {
 // return:
 // response, statusCode, and error (if has)
 func (c *ClientBreaker) Do(ctx context.Context, args RequestArgs) Response {
-	cb := c.breaker.getCircuitBreaker(args.RequestURL)
+	cb := c.cb.getCircuitBreaker(args.RequestURL)
 
 	resp, err := cb.Execute(func() (interface{}, error) {
 		resp := c.client.Do(ctx, args)
